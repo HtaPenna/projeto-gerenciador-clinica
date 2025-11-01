3ca8752c4104e483cf502e2321f25ee34f678fe4
MauroCarmo <mbdc.carmo@gmail.com>
2025-11-01 10:19:04 -0300

    remove sair, add link ativo

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import './Navbar.css';
 import logo from "../../assets/images/logo.png";
-import { Home, Users, Calendar, LogOut, HeartPulse, User } from "lucide-react";
+import { Home, Users, Calendar, LogOut, HeartPulse, User, ClipboardCheck } from "lucide-react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const handleLogout = () => {
-    // Lógica de logout
-    navigate('/');
+  const isActive = (path) => {
+    if (path === '/main') {
+      return location.pathname === '/main' || location.pathname === '/main/';
+    } else {
+      return location.pathname.startsWith(path);
+    }
   };
 
   return (
     <div id="navContainer">
       <div id="logoNav">
-        <div class="iconNav"><img src={logo} alt="logo reduzida da dent.FY" /></div>
+        <div className="iconNav"><img src={logo} alt="logo reduzida da dent.FY" /></div>
       </div>
       <nav className="navbar">
         <div id='links'>
-          <Link to="/main"><Home size={24} class="iconNav"/><span>Início</span></Link>
-          <Link to="/main/pacientes"><Users size={24} class="iconNav"/><span>Pacientes</span></Link>
-          <Link to="/main/agenda"><Calendar size={24} class="iconNav"/><span>Agenda</span></Link>
-          <Link to="/main/estoque"><Calendar size={24} class="iconNav"/><span>Estoque</span></Link>
+          <Link to="/main" className={isActive('/main') ? 'nav-link active' : 'nav-link'}>
+            <Home size={24} className="iconNav"/>
+            <span>Início</span>
+          </Link>
+          <Link to="/main/pacientes" className={isActive('/main/pacientes') ? 'nav-link active' : 'nav-link'}>
+            <Users size={24} className="iconNav"/>
+            <span>Pacientes</span>
+          </Link>
+          <Link to="/main/agenda" className={isActive('/main/agenda') ? 'nav-link active' : 'nav-link'}>
+            <Calendar size={24} className="iconNav"/>
+            <span>Agenda</span>
+          </Link>
+          <Link to="/main/estoque" className={isActive('/main/estoque') ? 'nav-link active' : 'nav-link'}>
+            <ClipboardCheck size={24} className="iconNav"/>
+            <span>Estoque</span>
+          </Link>
         </div>
-        <div id="btnUserOptions">
-          <User size={24} class="iconNav"/><span>Minha conta</span>
-        </div>
-        <button onClick={handleLogout} className="logout-btn">
-          <LogOut size={24}/><span>Sair</span>
-        </button>
       </nav>
     </div>
   );
