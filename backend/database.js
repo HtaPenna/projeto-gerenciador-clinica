15e8b41d0d3287e39ebfc2d73402a06d84130382
MauroCarmo <mbdc.carmo@gmail.com>
2025-11-14 19:42:33 -0300

    atualização do database

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,16 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('sistema_odontologico', 'root', '1234', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER, 
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: 3307,
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
